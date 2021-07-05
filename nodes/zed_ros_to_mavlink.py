f777aa3b25cb20f1ae9641876bbf91ecae9bf48d
mtbsteve <schiwo1@gmail.com>
2021-07-05 08:37:15 +0200

    fixed timestamp to milliseconds

--- nodes/zed_ros_to_mavlink.py
+++ nodes/zed_ros_to_mavlink.py
@@ -94,6 +94,11 @@ depth_vfov_deg = None
 vehicle_pitch_rad = None
 data = None
 current_time_us = 0
+start_time =  int(round(time.time() * 1000))
+current_milli_time = lambda: int(round(time.time() * 1000) - start_time)
+current_time_ms = current_milli_time()
+print(current_time_ms)
+current_time_ms = current_milli_time()
 last_obstacle_distance_sent_ms = 0  # value of current_time_us when obstacle_distance last sent
 
 # Obstacle distances in front of the sensor, starting from the left in increment degrees to the right
@@ -187,7 +192,7 @@ def send_obstacle_distance_message():
             progress("no new frame")
             return
         last_obstacle_distance_sent_ms = current_time_us
-        print(distances)
+        #print(distances)
         if angle_offset is None or increment_f is None:
             progress("obstacle_distance_params not properly set")
         else:
@@ -212,7 +217,7 @@ def send_distance_sensor_message():
     # Average out a portion of the centermost part
     if (enable_msg_distance_sensor == True):
         curr_dist = int(np.mean(distances[34:38]))
-        print(curr_dist)
+        #print(curr_dist)
         conn.mav.distance_sensor_send(
             0,# ms Timestamp (UNIX time or time since system boot) (ignored)
             min_depth_cm,   # min_distance, uint16_t, cm
@@ -228,19 +233,21 @@ def send_distance_sensor_message():
 def send_obstacle_distance_3D_message():
     global mavlink_obstacle_coordinates, min_depth_cm, max_depth_cm
     global last_obstacle_distance_sent_ms
-    global current_time_us
+    global current_time_ms
     if (enable_3D_msg_obstacle_distance == True):
-        if current_time_us == last_obstacle_distance_sent_ms:
+        if current_time_ms == last_obstacle_distance_sent_ms:
             # no new frame
             progress("no new frame")
             return
-        last_obstacle_distance_sent_ms = current_time_us
-        print(mavlink_obstacle_coordinates)
-        current_time_us = int(round(time.time() * 1000000))
+        last_obstacle_distance_sent_ms = current_time_ms
+
         for q in range(9):
-            print("we arein the 3d loop", q)
+            #print(float(mavlink_obstacle_coordinates[q][0]),
+            #    float(mavlink_obstacle_coordinates[q][1]),
+            #    float(mavlink_obstacle_coordinates[q][2]))
+
             conn.mav.obstacle_distance_3d_send(
-                current_time_us,    # us Timestamp (UNIX time or time since system boot)
+                current_time_ms,    # ms Timestamp (UNIX time or time since system boot)
                 0,
                 mavutil.mavlink.MAV_FRAME_BODY_FRD,
                 65535,
@@ -250,6 +257,7 @@ def send_obstacle_distance_3D_message():
                 float(min_depth_cm/100),
                 float(max_depth_cm/100) #needs to be in meters
             )
+        current_time_ms = current_milli_time()
 
 def send_msg_to_gcs(text_to_be_sent):
     # MAV_SEVERITY: 0=EMERGENCY 1=ALERT 2=CRITICAL 3=ERROR, 4=WARNING, 5=NOTICE, 6=INFO, 7=DEBUG, 8=ENUM_END
@@ -384,6 +392,7 @@ sched.start()
 last_time = time.time()
 # Store the timestamp for MAVLink messages
 current_time_us = int(round(time.time() * 1000000))
+current_time_ms = current_milli_time()
 rospy.spin()
 
 mavlink_thread_should_exit = True
